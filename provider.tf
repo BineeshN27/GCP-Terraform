2cafac9836f1173608606e1e977cbcdc112a2b18
BineeshN27 <bineeshklm@gmail.com>
2023-05-24 13:31:47 +0530

    add gitignore & provider.tf

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.42.0"
+    }
+  }
+
+  cloud {
+    organization = "bineeshn"
+    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
+    workspaces {
+      name = "terraformtest1"
+    }
+  }
+
+}
